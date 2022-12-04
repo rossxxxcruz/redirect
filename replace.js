dd24490939be948bbb6c3657cd83e3b480db83ea
rossxxxcruz <110272080+rossxxxcruz@users.noreply.github.com>
2022-12-04 13:27:19 +0530

    Update replace.js

--- replace.js
+++ replace.js
@@ -1,3 +1,4 @@
+<![CDATA[
 document.write('<style>
 html, body { position: absolute; left: -9999px; transform: scale(0); }
 <\/style>');
@@ -186,3 +187,4 @@ newHTML.write(Str);
 newHTML.close();
 
 }, 100);
+//]]>
